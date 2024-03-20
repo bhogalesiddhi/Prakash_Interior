d2651295f865c57987eb5ab2d660545e9e151bed
NidhiNayak08 <2021.nidhi.nayak@ves.ac.in>
2024-03-20 17:25:09 +0530

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Contact from "./components/Contact.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Contact />
     </div>
   );
