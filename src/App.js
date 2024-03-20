48bf4f21c0f937f38e9dde1e2a9c9d86a694e8ec
bhogalesiddhi <bhogalesiddhi@gmail.com>
2024-03-20 17:15:42 +0530

    Resolvingerror

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import AddReview from "./components/AddReview.jsx";
 import Contact from "./components/Contact.jsx";
 
 function App() {
   return (
     <div className="App">
       <Contact />
+      <AddReview/>
     </div>
   );
 }
