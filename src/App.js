700a217f21e77d5d8516a1fcdc31c743689568bb
bhogalesiddhi <bhogalesiddhi@gmail.com>
2024-04-13 19:19:52 +0530

    Hero Section

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import AddReview from "./components/AddReview.jsx";
 import Contact from "./components/Contact.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Home from "./components/Home.jsx";
+import Hero from "./components/Hero/Hero.jsx";
+import NewNavbar from "./components/NewNavbar/NewNavbar.jsx";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
+        {/* <Navbar /> */}
         <Routes>
+          <Route path="/newNavbar" element={<NewNavbar/>}></Route>
+          <Route path="/hero" element={<Hero/>}/>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
+          <Route></Route>
         </Routes>
       </div>
     </Router>
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import "./Hero.css"
+import NewNavbar from '../NewNavbar/NewNavbar'
+
+
+const Hero = () => {
+  return (
+    <div className='main'>
+      <div className='nav'>
+      <NewNavbar/>
+      </div>
+     
+    <div className='mainContainer'>
+        <div className='leftSide'>
+        <div className='quoteSection '>
+            <div className='quoteSection1 quote quote1'>
+            <span>ELEGANT</span>
+            <span>INTERIORS</span>
+            </div>
+            <div className='quoteSection2'>
+              <span>Discover bespoke interior designs that blend elegance with practicality</span>
+              <span>tailored to enhance the vibrancy and comfort </span>
+              <span>of modern city living.</span>
+            </div>
+        </div>
+        </div>
+        <div className='rightSide'>
+            <div className='rightsideimg'>
+
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Hero
